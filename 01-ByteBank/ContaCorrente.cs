676bed3f4d93ef786356b131b2de8aa851fdf70c
Ricardo Santos <ricardooliveirasantos.ap@gmail.com>
2020-05-13 23:29:04 -0300

    Criação do projeto ByteBank

---
+++ 01-ByteBank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿
+
+//Criando a classe do tipo ContaCorrente e definindo os atributos que julgar necessários
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo;
+}
---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criação de instância de classe, criaremos outro objeto do tipo ContaCorrente
+            //Instância contaDaGabriela da classe ContaCorrente
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+
+            //Criação de instância de classe, criaremos outro objeto do tipo ContaCorrente
+            //Instância contaDaGabrielaCosta da classe ContaCorrente
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+            contaDaGabrielaCosta.titular = "Gabriela";
+            contaDaGabrielaCosta.agencia = 863;
+            contaDaGabrielaCosta.numero = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaDaGabriela == contaDaGabrielaCosta));
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+
+            Console.WriteLine("Igualdade de tipo de valor: " + (idade == idadeMaisUmaVez));
+
+            contaDaGabriela = contaDaGabrielaCosta;
+            Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta);
+
+            contaDaGabriela.saldo = 300;
+            Console.WriteLine(contaDaGabrielaCosta.saldo);
+            Console.WriteLine(contaDaGabriela.saldo);
+
+
+            Console.ReadLine();
+        }
+    }
+}
